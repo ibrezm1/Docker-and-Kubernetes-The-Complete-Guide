8693ff0f1d7e0549bb0d117998ff803043a2b86e
ibrezm1 <ibrezm1@gmail.com>
2021-08-21 10:11:00 +0530

    updates

--- 05_Docker_Compose_with_Multiple_Local_Containers/visits/index.js
+++ 05_Docker_Compose_with_Multiple_Local_Containers/visits/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const redis = require("redis");
+const process = require("process")
 
 const app = express();
 const client = redis.createClient({
@@ -14,6 +15,15 @@ app.get("/", (req, res) => {
     client.set("visits", parseInt(visits) + 1);
   });
 });
+
+app.get("/crash", (req, res) => {
+  process.exit(0)
+  client.get("visits", (err, visits) => {
+    res.send("Number of visits is " + visits);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
+
 
 app.listen(8081, () => {
   console.log("Listening on port 8081");
